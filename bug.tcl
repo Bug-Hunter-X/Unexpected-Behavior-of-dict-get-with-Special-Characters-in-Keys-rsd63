ad88cd20b56cc90e79d41e6e4c292b8e736bc710
BugHunter-X <askzenai@gmail.com>
2025-01-12 04:50:05 +0100

    Add bug.tcl file

---
+++ bug.tcl
@@ -0,0 +1,12 @@
+proc get_value {key} { 
+  if {[dict exists $my_dict $key]} { 
+    return [dict get $my_dict $key] 
+  } else { 
+    return "default_value" 
+  }
+}
+
+set my_dict {a 1 b 2 c 3}
+puts [get_value a] ;#Output: 1
+puts [get_value d] ;#Output: default_value
+puts [get_value "a b"] ;#Output: default_value
